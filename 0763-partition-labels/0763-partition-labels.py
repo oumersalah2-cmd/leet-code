e8c8c983a26105f68c5d40a080dda975be47a114
Abdusalam Oumer <oumersalah2@gmail.com>
2026-04-23 21:43:10 +0300

    Time: 3 ms (83.4%), Space: 19.2 MB (65.91%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+       last_occurrence = {char: idx for idx, char in enumerate(s)}
+
+       result = []
+       start = 0
+       end = 0
+
+       for i, char in enumerate (s):
+            end = max (end, last_occurrence[char])
+
+            if i == end:
+                result.append(end - start + 1)
+                start = i+1
+
+       return result
